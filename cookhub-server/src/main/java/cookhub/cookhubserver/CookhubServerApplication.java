38c6aa23b36a1a2a4d51724b4f7dc9664890b4ee
JunseoHo <112461939+JunseoHo@users.noreply.github.com>
2023-12-18 17:29:17 +0900

    feat : server

---
+++ cookhub-server/src/main/java/cookhub/cookhubserver/CookhubServerApplication.java
@@ -0,0 +1,13 @@
+package cookhub.cookhubserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookhubServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookhubServerApplication.class, args);
+    }
+
+}
