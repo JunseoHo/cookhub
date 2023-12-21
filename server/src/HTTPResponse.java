a34727566a3de6abfb3ba212ffb3faf617cf3d5f
JunseoHo <112461939+JunseoHo@users.noreply.github.com>
2023-12-21 16:14:02 +0900

    feat : welcome page

---
+++ server/src/HTTPResponse.java
@@ -0,0 +1,24 @@
+package main;
+
+public class HTTPResponse {
+
+    public String httpVersion = "";
+    public int statusCode = -1;
+    public String reasonPhrase = "";
+    public String contentType = "";
+    public String responseBody = "";
+
+    @Override
+    public String toString() {
+        String httpResponse = "";
+        // start line
+        httpResponse += httpVersion + " " + statusCode + " " + reasonPhrase + "\n";
+        // header
+        httpResponse += "Content-Type: " + contentType + "\n";
+        httpResponse += "\n";
+        // body
+        httpResponse += responseBody;
+        return httpResponse;
+    }
+
+}
--- server/src/Main.java
+++ server/src/Main.java
@@ -1,19 +1,19 @@
 package main;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
 
-
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+        try (ServerSocket serverSocket = new ServerSocket(40055)) {
             System.out.println("Server socket open.");
             while (true) {
+                System.out.println("Server socket is waiting...");
                 Socket socket = serverSocket.accept();
+                System.out.println("Request is accepted.");
                 new Thread(() -> handleRequest(socket)).start();
             }
         } catch (IOException e) {
@@ -21,11 +21,30 @@ public class Main {
         }
     }
 
+    private static String readFile(String path) {
+        File file = new File(path);
+        if (!file.exists()) return null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String content = "";
+            String line;
+            while ((line = reader.readLine()) != null) content += line + "\n";
+            return content;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
     private static void handleRequest(Socket socket) {
         try {
             OutputStream outputStream = socket.getOutputStream();
-            String response = "HTTP/1.1 200 OK\r\nContent-Type: text/hi\r\n\r\nHello, World!";
-            byte[] responseBytes = response.getBytes(StandardCharsets.UTF_8);
+            HTTPResponse httpResponse = new HTTPResponse();
+            httpResponse.httpVersion = "HTTP/1.1";
+            httpResponse.statusCode = 200;
+            httpResponse.reasonPhrase = "OK";
+            httpResponse.contentType = "text/html";
+            httpResponse.responseBody = readFile("server/resources/welcome.html");
+            System.out.println(httpResponse);
+            byte[] responseBytes = httpResponse.toString().getBytes(StandardCharsets.UTF_8);
             outputStream.write(responseBytes);
             outputStream.close();
             socket.close();
