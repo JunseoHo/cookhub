3250eae8d4f659f7fa00ad25fd7d5ba0352bf4b2
JunseoHo <112461939+JunseoHo@users.noreply.github.com>
2023-12-22 19:55:35 +0900

    refactor : HttpResponse.java

--- server/src/http/HttpResponse.java
+++ server/src/http/HttpResponse.java
@@ -2,23 +2,41 @@ package main.http;
 
 public class HttpResponse {
 
-    public String httpVersion = "";
-    public int statusCode = -1;
-    public String reasonPhrase = "";
-    public String contentType = "";
-    public String responseBody = "";
+    private String httpVersion = null;
+    private Integer statusCode = null;
+    private String reasonPhrase = null;
+    private String contentType = null;
+    private String responseBody = null;
+
+    public void setHttpVersion(String httpVersion) {
+        this.httpVersion = httpVersion;
+    }
+
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public void setReasonPhrase(String reasonPhrase) {
+        this.reasonPhrase = reasonPhrase;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public void setResponseBody(String responseBody) {
+        this.responseBody = responseBody;
+    }
 
     @Override
     public String toString() {
-        String httpResponse = "";
-        // start line
-        httpResponse += httpVersion + " " + statusCode + " " + reasonPhrase + "\n";
-        // header
-        httpResponse += "Content-Type: " + contentType + "\n";
-        httpResponse += "\n";
-        // body
-        httpResponse += responseBody;
-        return httpResponse;
+        if (httpVersion == null || statusCode == null || reasonPhrase == null) return null;
+        StringBuilder httpResponse = new StringBuilder();
+        httpResponse.append(httpVersion).append(' ').append(statusCode).append(' ').append(reasonPhrase).append('\n');
+        if (contentType != null) httpResponse.append("Content-Type: ").append(contentType).append('\n');
+        httpResponse.append('\n');
+        httpResponse.append(responseBody);
+        return httpResponse.toString();
     }
 
 }
