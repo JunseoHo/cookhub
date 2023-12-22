ae942e16fca88a7fbeb1d07a8fbd87304f8973a0
kimsejin <kimsejin2118@gmail.com>
2023-12-22 16:15:46 +0900

    feat : parse http reqeust

--- server/src/Main.java
+++ server/src/Main.java
@@ -34,32 +34,27 @@ public class Main {
         }
     }
 
+
     private static void handleRequest(Socket socket) {
         try {
-
             // 입력 스트림을 통해 클라이언트의 요청 읽기
             InputStream inputStream = socket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-
             // HTTP 요청 헤더 읽기
             StringBuilder requestStringBuilder = new StringBuilder();
             String line;
             while ((line = reader.readLine()) != null && !line.isEmpty()) {
                 requestStringBuilder.append(line).append("\n");
             }
-
             // 요청 정보 출력
-            System.out.println("Received HTTP Request:\n" + requestStringBuilder.toString());
-
+            String request = requestStringBuilder.toString();
+            System.out.println("Received HTTP Request:\n" + request);
+            String fileName = request.split("\n")[0].split(" ")[1];
+            if (fileName.equals("/")) fileName = "welcome.html";
+            System.out.println("file name : " + fileName);
+            String fileExtension = fileName.split("\\.")[1];
             OutputStream outputStream = socket.getOutputStream();
-            HTTPResponse httpResponse = new HTTPResponse();
-            httpResponse.httpVersion = "HTTP/1.1";
-            httpResponse.statusCode = 200;
-            httpResponse.reasonPhrase = "OK";
-            httpResponse.contentType = "text/html";
-            httpResponse.responseBody = readFile("server/resources/welcome.html");
-            byte[] responseBytes = httpResponse.toString().getBytes(StandardCharsets.UTF_8);
-            outputStream.write(responseBytes);
+            make_response(fileExtension, "server/resources/"+fileName, outputStream);
             outputStream.close();
             socket.close();
         } catch (IOException e) {
@@ -67,4 +62,15 @@ public class Main {
         }
     }
 
+    private static void make_response(String fileExtension, String filePath, OutputStream outputStream) throws IOException {
+        HTTPResponse httpResponse = new HTTPResponse();
+        httpResponse.httpVersion = "HTTP/1.1";
+        httpResponse.statusCode = 200;
+        httpResponse.reasonPhrase = "OK";
+        httpResponse.contentType = "text/" + fileExtension;
+        httpResponse.responseBody = readFile(filePath);
+        byte[] responseBytes = httpResponse.toString().getBytes(StandardCharsets.UTF_8);
+        outputStream.write(responseBytes);
+        outputStream.flush();
+    }
 }
